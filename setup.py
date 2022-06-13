447fcdabdee6e26c98f0a5803d556d30c9eaa6d6
Dmitry Orlov <me@mosquito.su>
2022-06-13 23:39:23 +0300

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="lxd-exporter",
-    version="0.4.3",
+    version="0.4.4",
     include_package_data=True,
     license="Apache Software License",
     author="Dmitry Orlov",
